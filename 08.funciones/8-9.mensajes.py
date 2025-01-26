5015fc78a9c51b37d40c1a190f11f9c6058a7dff
diadasiachilensis <guillermo.vidal.astudillo@gmail.com>
2025-01-26 18:31:18 -0300

    funciones 9

---
+++ 08.funciones/8-9.mensajes.py
@@ -0,0 +1,8 @@
+msn=['hola como estas','cuando se van','llego enseguida','ya he llegado']
+
+def mostrar_mensaje(messages):
+    for message in messages:
+        msg=f'mensaje por mostrar: {message}'
+        print(msg)
+
+mostrar_mensaje(msn)
